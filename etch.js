e272d52da899738cdd86ef09cf0604b8734dbca0
Sabrina <1771581+sabs@users.noreply.github.com>
2023-07-08 13:44:48 +0100

    Working version with colour change and grid size change. Looking at how it functions on mobile now

--- etch.js
+++ etch.js
@@ -1,6 +1,7 @@
 
 function displayGrid(gridSize) {
     const container = document.getElementById('container');
+    container.innerHTML = "";
 
     for (let x = 0; x < (gridSize); x++) {
         let newdiv = document.createElement('div');
@@ -10,7 +11,32 @@ function displayGrid(gridSize) {
         }
         container.appendChild(newdiv);
     }    
-    
+    container.addEventListener('touchmove', paintSquareTouch);
+    container.addEventListener('mouseover', paintSquare);
 }
 
-displayGrid(64);
+function paintSquare(e) {
+    e.target.style.backgroundColor = getBackgroundColor();
+}
+
+
+function getBackgroundColor() {
+    const colorPicker = document.getElementById('color');
+    return colorPicker.value;
+}
+
+function paintSquareTouch(e) {
+    let touch = e.touches[0];
+    let square = document.elementFromPoint(touch.clientX, touch.clientY);
+    square.style.backgroundColor = getBackgroundColor();
+}
+
+// this.color.addEventListener('change', setBackgroundColor, false);
+this.gridSizeButton.addEventListener('click', function (e) {
+    const gridSize = document.getElementById('gridSize').value;
+    if (gridSize >= 2 && gridSize <= 100) {
+        displayGrid(gridSize);
+    }
+})
+displayGrid(16);
+
